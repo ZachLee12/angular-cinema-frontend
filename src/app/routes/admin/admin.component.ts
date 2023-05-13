a43e0f1fdacb6ba824547c31f70b3fea5839d0f6
ZachLee12 <leezhengyang22@gmail.com>
2023-05-13 19:18:58 +0200

    NgModel separated between CRUD functions

--- src/app/routes/admin/admin.component.ts
+++ src/app/routes/admin/admin.component.ts
@@ -10,6 +10,8 @@ import { DatabaseService } from 'src/app/core/services/database/database.service
 export class AdminComponent {
   movies: string[] = []
   movieName: string = '';
+  movieNameToAdd: string = '';
+  movieNameToDelete: string = '';
   movieTime: string = '';
   serverResponse?: any;
   constructor(private httpClient: HttpClient, private databaseService: DatabaseService) {
@@ -31,15 +33,15 @@ export class AdminComponent {
   }
 
   addMovie() {
-    this.movies.push(this.movieName) //to update the UI without rerendering the page
-    this.databaseService.addMovie(this.movieTime, this.movieName).subscribe(response => {
+    this.movies.push(this.movieNameToAdd) //to update the UI without rerendering the page
+    this.databaseService.addMovie(this.movieTime, this.movieNameToAdd).subscribe(response => {
       this.serverResponse = response
     })
   }
 
   deleteMovie() {
-    this.movies = this.movies.filter(name => this.movieName !== name)//to update the UI without rerendering the page
-    this.databaseService.deleteMovie(this.movieName).subscribe(response => {
+    this.movies = this.movies.filter(name => this.movieNameToDelete !== name)//to update the UI without rerendering the page
+    this.databaseService.deleteMovie(this.movieNameToDelete).subscribe(response => {
       this.serverResponse = response
     })
   }
