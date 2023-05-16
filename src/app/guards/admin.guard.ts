14574887f280deaea1dc8846a06a06db1bf5f385
ZachLee12 <leezhengyang22@gmail.com>
2023-05-16 14:52:11 +0200

    Fix admin guard

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,15 +1,13 @@
-import { Injectable, inject } from '@angular/core';
+import { inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
 
 export const AdminGuard: CanActivateFn = () => {
   const router = inject(Router)
-  // let adminPassword = prompt("Admin Password:", "password")
-  // if (adminPassword === "password") {
-  //   return true;
-  // } else {
-  //   router.navigateByUrl("/")
-  //   return false
-  // }
-
-  return true;
+  let adminPassword = prompt("Admin Password:", "password")
+  if (adminPassword === "password") {
+    return true;
+  } else {
+    router.navigateByUrl("/")
+    return false
+  }
 }
