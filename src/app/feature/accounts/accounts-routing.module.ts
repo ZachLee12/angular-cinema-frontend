a11fdd34fdcf152caa9736260a29635b35538a8c
ZachLee12 <leezhengyang22@gmail.com>
2023-07-04 19:43:14 +0200

    Style signup and login nav btns

--- src/app/feature/accounts/accounts-routing.module.ts
+++ src/app/feature/accounts/accounts-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './components/sign-up/components/sign-up/sign-up.component';
 import { LoginPageComponent } from './components/login/components/login-page/login-page.component';
+import { AccountsComponent } from './components/accounts/accounts.component';
 
 const routes: Routes = [
     {
         path: 'accounts',
+        component: AccountsComponent,
         children: [
             {
                 path: 'sign-up',
