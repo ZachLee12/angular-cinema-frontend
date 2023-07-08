6bb02caca50288c5db589d988a61e02e03acdacb
ZachLee12 <leezhengyang22@gmail.com>
2023-07-08 16:26:46 +0200

    Add angular animations to carousel

--- src/app/routes/main-page/components/carousel/carousel.component.ts
+++ src/app/routes/main-page/components/carousel/carousel.component.ts
@@ -1,10 +1,61 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate(500, style({ opacity: 1 })),
+      ])
+    ]),
+  ]
 })
 export class CarouselComponent {
+  currentIndex: number = 0
+  slides: any[] = [
+    {
+      url: 'http://placekitten.com/400/200',
+      display: true
+    },
+    {
+      url: 'http://placekitten.com/400/202',
+      display: false
+    },
+    {
+      url: 'http://placekitten.com/400/203',
+      display: false
+    },
+    {
+      url: 'http://placekitten.com/400/205',
+      display: false
+    },
+  ]
+
+  next() {
+    if (this.currentIndex === this.slides.length - 1) {
+      this.currentIndex = 0;
+    }
+    else if (this.currentIndex === -1) {
+      this.currentIndex = this.slides.length
+    } else {
+      this.currentIndex += 1;
+    }
+    this.slides.forEach(s => s.display = false)
+    this.slides[this.currentIndex].display = true
+  }
+
+  prev() {
+    if (this.currentIndex === 0) {
+      this.currentIndex = this.slides.length - 1
+    } else {
+      this.currentIndex -= 1;
+    }
+    this.slides.forEach(s => s.display = false)
+    this.slides[this.currentIndex].display = true
+  }
 
 }
