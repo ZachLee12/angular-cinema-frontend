3ccb4e43a2b96b408db72e9e04e45f246e1384a5
ZachLee12 <leezhengyang22@gmail.com>
2023-07-21 21:50:39 +0200

    Merge route params into one observable BookingComponent

--- src/app/feature/booking/booking/booking.component.ts
+++ src/app/feature/booking/booking/booking.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { MovieService } from 'src/app/core/services/movie/movie-service.service';
-import { Observable, switchMap, tap } from 'rxjs'
+import { Observable, combineLatestWith, map, mergeMap, of, switchMap, tap } from 'rxjs'
 import { Movie } from '../../movie/interfaces';
 import { ActivatedRoute, Params } from '@angular/router';
 
@@ -13,17 +13,56 @@ export class BookingComponent {
   movieService: MovieService = inject(MovieService)
   activatedRoute: ActivatedRoute = inject(ActivatedRoute)
 
-  currentMovie$!: Observable<Movie>;
   currentRouteParams$?: Observable<Params>;
-  halls$?: Observable<any>;
+  currentMovie$!: Observable<Movie>;
+  hallInfo$?: Observable<any>;
+  seats?: any[]
+
+  //dummy Array for Angular template to loop through to generate seats
+  tenElementsArray: any[] = new Array(10).fill('')
+  sevenElementsArray: any[] = new Array(7).fill('')
 
   ngOnInit() {
-    // this.movieService.getOneMovie$("8c6e9f31-f5e4-4c2a-be26-99ad1204ba72")
-    //   .pipe(
-    //     tap(movie => { this.movieService.setCurrentMovie$(movie) })
-    //   ).subscribe()
+    this.movieService.getOneMovie$("8c6e9f31-f5e4-4c2a-be26-99ad1204ba72")
+      .pipe(
+        tap(movie => { this.movieService.setCurrentMovie$(movie) })
+      ).subscribe()
 
     this.currentMovie$ = this.movieService.getCurrentMovie$()
+    this.hallInfo$ = this.activatedRoute.params
+      .pipe(
+        combineLatestWith(this.activatedRoute.queryParams),
+        map(([params, queryParams]) => {
+          return {
+            ...params,
+            ...queryParams
+          }
+        }),
+        tap((combinedData: any) => this.getArrayForHallSizeToSeats(combinedData.hallSize))
+      )
 
+  }
+
+  getArrayForHallSizeToSeats(hallSize: string): any[] {
+    let numOfSeats;
+    let seatsArray: any[] = []
+    switch (hallSize) {
+      case 'BIG':
+        numOfSeats = 75
+        break
+      case 'MEDIUM':
+        numOfSeats = 50
+        break
+      case 'SMALL':
+        numOfSeats = 25
+        break
+      default:
+        numOfSeats = 0
+    }
+    for (let i = 0; i < numOfSeats; i++) {
+      seatsArray.push({ seatId: i, booked: false, selected: false })
+    }
+    this.seats = seatsArray
+    return seatsArray
   }
 }
