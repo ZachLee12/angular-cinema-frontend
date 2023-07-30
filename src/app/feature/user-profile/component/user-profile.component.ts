1d15754e20b54f6aa819c238eeb7ac865ec53e7a
ZachLee12 <leezhengyang22@gmail.com>
2023-07-30 12:29:00 +0200

    Add toggle and close icons for sidebar

--- src/app/feature/user-profile/component/user-profile.component.ts
+++ src/app/feature/user-profile/component/user-profile.component.ts
@@ -1,9 +1,5 @@
-import { Component, inject } from '@angular/core';
-import { BehaviorSubject, Observable, Subject, filter, skip, switchMap, tap } from 'rxjs';
-import { UserProfile } from 'src/app/core/services/login/interfaces';
-import { LoginService } from 'src/app/core/services/login/login.service';
-import { UserProfileService } from '../service/user-profile.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -11,21 +7,10 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent {
-  loginService: LoginService = inject(LoginService)
-  userProfileService: UserProfileService = inject(UserProfileService)
+  renderSidebar: boolean = true
+  renderCloseIcon: boolean = false
 
-  userProfile$?: Observable<any>
-  userBookings$?: Observable<any>
-
-
-  ngOnInit() {
-    this.userProfile$ = this.loginService.getTokenUserProfile$()
-      .pipe(filter(({ username }) => username !== ''), switchMap(({ username }) => this.userProfileService.getUserProfile$(username)))
-
-    this.userBookings$ = this.loginService.getTokenUserProfile$()
-      .pipe(filter(({ id }) => id !== ''), switchMap(({ id }) => this.userProfileService.getUserBookings$(id)))
+  toggleSidebar() {
+    this.renderSidebar = !this.renderSidebar
   }
-
-
-
 }
